a80104dbe7c759cb91ffafb50faba38cba0160c0
laits1 <thsehdrl1@gmail.com>
2021-10-28 12:10:01 +0900

    2021년 10월 28일 목 오후 12:09:35

--- Neflex/main/views.py
+++ Neflex/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from requests import get
 from bs4 import BeautifulSoup as Soup
+import pandas as pd
 
 # Create your views here.
 def index(request):
@@ -16,6 +17,9 @@ def index(request):
         'image': image,
         'movie_id': movie_id
     }
+
+    # movie_id2 = 2382321
+    # image2 = movies[0].div.img['srcset'].split(',')[-4]
     
     return render(request, "main/index.html", context=context)
 
@@ -25,7 +29,22 @@ def about(request):
 
 
 def movie(request, movie_id):
+
+    url = get('https://www.imdb.com/title/tt2382320/')
+    req = url.text
+    soup_data = Soup(req, 'html.parser')
+    movies = soup_data.findAll('div', {'class' : 'ipc-poster ipc-poster--baseAlt ipc-poster--dynamic-width Poster__CelPoster-sc-6zpm25-0 kPdBKI celwidget ipc-sub-grid-item ipc-sub-grid-item--span-2'})
+    movie_id = 2382320
+    movie_df = pd.read_csv("C:/Users/SDG/Documents/Github/3rd_pj_django/movies.csv")
+   
+
+    # title =  soup_data.findAll('div', {'class' : 'TitleBlock__SeriesParentLinkWrapper-sc-1nlhx7j-3 itQvtY'})
+    # movie_title = "007 노 타임 투 다이"
+    movie_title = movie_df.loc[movie_df["imdb_movie_Id"] == movie_id, "title"]
+    image = movies[0].div.img['srcset'].split(',')[-4]
     context = {
-        'movie_id': movie_id
+        'image': image,
+        'movie_id': movie_id,
+        'movie_title' : movie_title[0]
     }
     return render(request, 'main/movie.html', context=context)
